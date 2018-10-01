8fbe52edbd5d24139c06ecb50d401ed5f8cd6c82
Guo <hguo@labs.neit.edu>
2018-10-01 13:45:16 -0400

    Hello World

---
+++ helloWorld.php
@@ -0,0 +1,14 @@
+<?php
+    $echo = "Hello World";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $echo; ?></title>
+    </head>
+    <body>
+        <?php echo $echo; ?>
+    </body>
+</html>
